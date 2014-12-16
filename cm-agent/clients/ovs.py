833f4d07ab6d44a58c8c9ea354796a4bf9d5bbd5
manue1 <manub1312@gmail.com>
2014-12-16 17:40:59 +0100

    Add basic version of ovs-vsctl commands.

--- cm-agent/clients/ovs.py
+++ cm-agent/clients/ovs.py
@@ -20,33 +20,34 @@ class Client(object):
         subprocess.check_call(["sudo", "ovs-vsctl", "set-manager", "ptcp:6640"])
         pass
 
-    def get_portid(self):
+    def get_port(self):
         pass
 
-    def add_port(self):
-        pass
-
-    def mod_port(self):
-        pass
-
-    def del_port(self):
-        pass
+    def set_port(self, port_id, action, action_id):
+        subprocess.check_call(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "set", "port", "%s" % port_id, "%s=%s" % (action, action_id)])
 
     def list_ports(self):
-        out = subprocess.check_output(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "list", "port"])
-        return out
+        ports = subprocess.check_output(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "list", "port"])
+        return ports
 
-    def add_queue(self):
-        pass
+    def create_queue(self, min_rate, max_rate):
+        queue_id = subprocess.check_output(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "create", "queue", "type=linux-htb", "other-config:min-rate=%d" % min_rate, "other-config:max-rate=%d" % max_rate])
+        return queue_id
 
-    def del_queue(self):
-        pass
+    def del_queue(self, queue_id):
+        subprocess.check_call(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "destroy", "queue", "%s" % queue_id])
 
-    def add_qos(self):
-        pass
+    def list_queue(self):
+        queues = subprocess.check_output(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "list", "queue"])
+        return queues
 
-    def del_qos(self):
-        pass
+    def create_qos(self, queue_in, queue_out):
+        qos_id = subprocess.check_output(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "create", "qos", "type=linux-htb", "queues=0=%s,1=%s" % (queue_in, queue_out) ])
+        return qos_id
+
+    def del_qos(self, qos_id):
+        subprocess.check_call(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "destroy", "qos", "%s" % qos_id])
 
     def list_qos(self):
-        pass
+        qoss = subprocess.check_output(["sudo", "ovs-vsctl", "--db=tcp:%s:6640" % self.host_ip, "list", "qos"])
+        return qoss
